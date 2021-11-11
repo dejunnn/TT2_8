49c58ffc0a439106631bd9e018bec73eebc34f47
dejunnn <e0324575@u.nus.edu>
2021-11-11 12:29:51 +0800

    Added Entity Relationship

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
 import os
+#basedir = os.path.abspath(os.path.dirname(__insert file here__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('KEY') or 'admin'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(__insertbasedirhere_, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
